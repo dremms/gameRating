393299fbf4fdbc1105e2e5bb3022d788a7c9ee03
Dremm <loic.coupliere@gmail.com>
2026-04-28 20:25:21 +0200

    fix: delete agree terms checkbox in register + delete comment in UserRepository.php

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -40,17 +40,6 @@ class RegistrationController extends AbstractController
             $entityManager->persist($user);
             $entityManager->flush();
 
-            // generate a signed url and email it to the user
-            $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
-                (new TemplatedEmail())
-                    ->from(new Address('gamerating@dremm.fr', 'gameRating'))
-                    ->to((string) $user->getEmail())
-                    ->subject('Please Confirm your Email')
-                    ->htmlTemplate('registration/confirmation_email.html.twig')
-            );
-
-            // do anything else you need here, like send an email
-
             return $security->login($user, 'form_login', 'main');
         }
 
